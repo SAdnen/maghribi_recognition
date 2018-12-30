2c26df94c32d920d4793f0f5a9d0752fa228684b
CAPFI TECH <capfitech@CAPFIGROUP.DOMAIN.com>
2018-12-30 23:22:59 +0100

    first commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+DIR_DATA_RAW = os.path.join('..', 'data', 'raw')
+DIR_DATA_INTERIM = os.path.join('..', 'data', 'interim')
+DIR_DATA_PROCESSED = os.path.join('..', 'data', 'processed')
+DIR_DATA_EXTERNAL = os.path.join('..', 'data', 'external')
+
+NAME_FILE_KHALIL = 'code_musulman_par_khalil.pdf'
+FILE_DATA_RAW_KHALIL = os.path.join(DIR_DATA_RAW, NAME_FILE_KHALIL)
