fa117bfd24e343f6f129b8ce60a3873023711531
Adnen SAADAOUI <adnen.saadaoui@vneuron.tn>
2019-01-02 23:29:25 +0100

    fix typos

--- src/data/convert_pdf_to_images.py
+++ src/data/convert_pdf_to_images.py
@@ -1,9 +1,10 @@
+import os
 import settings
 from pdf2image import convert_from_path
 
 DPI = 300
 PPM_OUTPUT_PATH = settings.DIR_KHALIL_PPM
-JPG_OUTPUT_PATH = setting.DIR_KHALIL_JPG
+JPG_OUTPUT_PATH = settings.DIR_KHALIL_JPG
 FILE_PATH = settings.FILE_DATA_RAW_KHALIL
 
 pages = convert_from_path(FILE_PATH, DPI,PPM_OUTPUT_PATH)
