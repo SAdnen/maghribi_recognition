c9be6793992eb5e75513451ee6f5664a6316a0f6
CAPFI TECH <capfitech@CAPFIGROUP.DOMAIN.com>
2019-01-02 22:54:07 +0100

    fix/settings, line segmentation

--- settings.py
+++ settings.py
@@ -1,9 +1,13 @@
 import os
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DIR_DATA_RAW = os.path.join('..', 'data', 'raw')
-DIR_DATA_INTERIM = os.path.join('..', 'data', 'interim')
-DIR_DATA_PROCESSED = os.path.join('..', 'data', 'processed')
-DIR_DATA_EXTERNAL = os.path.join('..', 'data', 'external')
+DIR_DATA_RAW = os.path.join(ROOT_DIR, 'data', 'raw')
+DIR_DATA_INTERIM = os.path.join(ROOT_DIR, 'data', 'interim')
+DIR_DATA_PROCESSED = os.path.join(ROOT_DIR, 'data', 'processed')
+DIR_DATA_EXTERNAL = os.path.join(ROOT_DIR, 'data', 'external')
 
 NAME_FILE_KHALIL = 'code_musulman_par_khalil.pdf'
 FILE_DATA_RAW_KHALIL = os.path.join(DIR_DATA_RAW, NAME_FILE_KHALIL)
+
+NAME_FILE_EXEMPLE = 'exemple.png'
+FILE_DATA_EXEMPLE = os.path.join(DIR_DATA_INTERIM, NAME_FILE_EXEMPLE)
---
+++ src/data/segment_image_to_sentence.py
@@ -0,0 +1,27 @@
+from settings import FILE_DATA_EXEMPLE, DIR_DATA_INTERIM
+import cv2
+import numpy as np
+import image
+import os
+
+img = cv2.imread(FILE_DATA_EXEMPLE)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+
+kernel = np.ones((5,100), np.uint8)
+img_dilation = cv2.dilate(thresh, kernel, iterations=1)
+
+im2, ctrs, hier = cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+for i, ctr in enumerate(sorted_ctrs):
+    # Get bounding box
+    x, y, w, h = cv2.boundingRect(ctr)
+
+    # Getting ROI
+    roi = image[y:y+h, x:x+w]
+
+    # show ROI
+    # path_name = os.path.join(DIR_DATA_INTERIM, 'segment no:'+str(i))
+    # cv2.imwrite(path_name, roi)
