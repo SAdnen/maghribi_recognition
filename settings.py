4e1d8a4db38d9bfd305370ca91973acdcd9fd415
Adnen SAADAOUI <adnen.saadaoui@vneuron.tn>
2019-01-02 23:13:35 +0100

    convert pdf to ppm, jpg

--- settings.py
+++ settings.py
@@ -6,8 +6,10 @@ DIR_DATA_INTERIM = os.path.join(ROOT_DIR, 'data', 'interim')
 DIR_DATA_PROCESSED = os.path.join(ROOT_DIR, 'data', 'processed')
 DIR_DATA_EXTERNAL = os.path.join(ROOT_DIR, 'data', 'external')
 
+
 NAME_FILE_KHALIL = 'code_musulman_par_khalil.pdf'
 FILE_DATA_RAW_KHALIL = os.path.join(DIR_DATA_RAW, NAME_FILE_KHALIL)
-
+DIR_KHALIL_JPG = os.path.join(DIR_DATA_INTERIM, 'Khalil_jpg')
+DIR_KHALIL_PPM = os.path.join(DIR_DATA_INTERIM, 'Khalil_ppm')
 NAME_FILE_EXEMPLE = 'exemple.png'
 FILE_DATA_EXEMPLE = os.path.join(DIR_DATA_INTERIM, NAME_FILE_EXEMPLE)
--- src/data/convert_pdf_to_images.py
+++ src/data/convert_pdf_to_images.py
@@ -1,1 +1,12 @@
 import settings
+from pdf2image import convert_from_path
+
+DPI = 300
+PPM_OUTPUT_PATH = settings.DIR_KHALIL_PPM
+JPG_OUTPUT_PATH = setting.DIR_KHALIL_JPG
+FILE_PATH = settings.FILE_DATA_RAW_KHALIL
+
+pages = convert_from_path(FILE_PATH, DPI,PPM_OUTPUT_PATH)
+
+for i, p in enumerate(pages):
+    p.save(os.path.join(JPG_OUTPUT_PATH, str(i)+'.jpg'), 'JPEG')
